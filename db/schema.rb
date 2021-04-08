89045b598e183afb88b7fe0e778a0f7ff9c9cf65
VitamiN <scoundrel98@gmail.com>
2021-04-08 13:57:02 +0300

    add product to table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_100441) do
+ActiveRecord::Schema.define(version: 2021_04_08_102102) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
