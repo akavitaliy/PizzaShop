49cb6b7162a658a8432230e7a8395d559850b715
VitamiN <scoundrel98@gmail.com>
2021-04-08 12:39:58 +0300

    add Rakefile, update Gemfile, add model Product

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require 'rubygems'
 require 'sinatra'
 require 'sqlite3'
 require 'sinatra/contrib'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizza.db"
+
+class Product < ActiveRecord::Base
+    
+end
 
 
 get '/' do 
